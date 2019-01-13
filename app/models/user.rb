43c79728aa37071844b2e86132c1e2f41819f502
mahi-a <anethalamahesh@gmail.com>
2019-01-13 15:55:31 +0530

    Overriding devise views

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   # Associations
   has_many :posts, dependent: :destroy
   has_many :permissions, dependent: :destroy
